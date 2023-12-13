202b55b418638c29ab2f5da5821ee83a7abb3c26
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-12-13 09:32:09 +0200

    refactoring

--- OpenWeather(API)/info_weather.py
+++ OpenWeather(API)/info_weather.py
@@ -6,3 +6,11 @@ class WeatherInfo:
     def get_temperature(self):
         # Returns the current temperature from weather data
         return self.data.get('main', {}).get('temp')
+
+    def get_min_temperature(self):
+        # Returns the minimal temperature from weather data
+        return self.data.get('main', {}).get('temp_min')
+
+    def get_max_temperature(self):
+        # Returns the maximal  temperature from weather data
+        return self.data.get('main', {}).get('temp_max')
